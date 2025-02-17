a9377a4a48c2f3e9d1639725c4675aedfea196cb
KonstantineKenchadze <konto.kencho@gmail.com>
2025-02-17 20:02:43 +0400

    second commit

---
+++ day 02/homework/hw2.py
@@ -0,0 +1,1 @@
+print("my hobbies are playing football, playing the piano, i also like coding , i can draw well and i like reading books in my free time(literraly once in a blue moon).I also like swimming and playing tennis.")
