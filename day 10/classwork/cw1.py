4acafdd62d1433c69a7ba0f1ea880ae985a45c94
KonstantineKenchadze <konto.kencho@gmail.com>
2025-04-27 18:16:06 +0400

    second commit

---
+++ day 10/classwork/cw1.py
@@ -0,0 +1,8 @@
+print(True and False or False and False or True or True or False)
+print(True)
+# 1. true and false = false
+# 2. false and false = false
+# 3. false or false = false
+# 4. false or true = true
+# 5. true or true = true
+# 6 true or false = true
---
+++ day 10/homework/hw2.py
@@ -0,0 +1,8 @@
+print(True and not False and (False or True) and True or not False)
+# 1 true and  not false = true
+# 2 false or true = true
+# 3 true and not false = true
+# 4 true and true = true
+# 5 true and true = true
+# 6 true or not false = true
+#  output = true
