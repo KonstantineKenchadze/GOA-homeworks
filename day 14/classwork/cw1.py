4fa2a819869c1ca0efa9805623331a658d86bfcf
KonstantineKenchadze <konto.kencho@gmail.com>
2025-05-21 21:39:01 +0400

    second commit

---
+++ day 14/classwork/cw1.py
@@ -0,0 +1,8 @@
+age=int(input("enter your age: "))
+exp=int(input("enter your expirience: "))
+height=int(input("enter your height: "))
+isHired=0
+if age>=18 and exp>2 and height>=175:
+  print("you are hired")
+else:
+ print("you are not hired.") 
---
+++ day 14/classwork/cw2.py
@@ -0,0 +1,8 @@
+x=int(input("how many people?"))
+time=int(input("enter time: "))
+lightsOn=0
+if x>2 and time >=7:
+    lightsOn=True
+    print(lightsOn)
+else:
+    print(False)
